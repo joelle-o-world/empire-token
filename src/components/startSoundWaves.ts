b000a0da1d7e38b9744b56986a57125dde08869f
Joel Plowright <joelyjoel@protonmail.com>
2021-05-28 13:22:05 +0100

    More flexible wave vertex data function

--- src/components/startSoundWaves.ts
+++ src/components/startSoundWaves.ts
@@ -13,9 +13,11 @@ attribute vec2 position;
 void main() {
   y1 = -sin(-position.x + 0.1 +phase);
   y2 = abs(0.5 - position.x) * sin(position.x * 7.0 + 0.5 * position.y - phase * 3.0);
+  y1 = y1 * y1;
+  y2 = y2 * y2;
   gl_Position = vec4(
     position.x, 
-    .5*(position.y * y2 + (1.0-position.y) * y1),
+    (position.y * y2 + (1.0-position.y) * y1) - 1.0,
     0.5, 
     1.0
   );
@@ -108,6 +110,41 @@ function makeGridBuffer({
     elementsPerVertex,
 
   };
+}
+
+function makeWaveVertices({
+  rows=50,
+  cols=50,
+  width=1,
+  xShift=0,
+  waveFunctions = [] as ((x:number) => number)[]
+}) {
+  const elementsPerVertex = waveFunctions.length + 1
+  const numberOfVertices = rows * cols
+  const size = elementsPerVertex * numberOfVertices
+  const buffer = new Float32Array(size);
+  const colWidth = width / (cols-1)
+  for(let row=0; row < rows; ++row) { 
+    // Zig zag column loops (To eliminate lines)
+    for(let d=0; d < cols; ++d) {
+      let col = (row % 2 === 0) ? d : cols - d - 1;
+      let x = colWidth * col + xShift
+      let vertexIndex = elementsPerVertex * (col * rows + row)
+      buffer[vertexIndex] = x
+      for(let i=0; i < waveFunctions.length; ++ i) {
+        let y = waveFunctions[i](x)
+        buffer[vertexIndex + 1 + i] = y
+      }
+    }
+  }
+
+  console.log("sound waves memory:", buffer.length * 4, "bytes")
+
+  return { 
+    vertexData: buffer,
+    elementsPerVertex,
+    numberOfVertices,
+  }
 }
 
 export default startSoundWaves;
